c04a7e5c1156d42e72dcf4715e186c91ac779001
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-16 01:52:32 -0400

    Convert number to reversed array of digits codewars problem

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 /**
  * This file will contain JS problems
  */
+
+
+ /**
+  * Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+  */
+
+const digitize = n => String(n).split('').reverse().map(e => Number(e))
