acff8f9ae5303a328dbffb0cc6bc89a5a6e9bba7
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-19 22:13:19 -0400

    remove the minimum from an array problem

--- js/codewars.js
+++ js/codewars.js
@@ -159,3 +159,16 @@ function gooseFilter(birds) {
 
   return birds.filter(bird => !geese.includes(bird));
 };
+
+/**
+ * Name: Remove the minimum
+ * Description: Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+ */
+
+function removeSmallest(numbers) {
+  let min = Math.min(...numbers);
+
+  numbers.splice(numbers.indexOf(min), 1);
+
+  return numbers;
+}
