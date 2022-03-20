5e4352b16d5e1b2ea9933094734f9e802a2e23b0
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-19 22:01:20 -0400

    more problems

--- js/thejsway.js
+++ js/thejsway.js
@@ -19,3 +19,17 @@ musketeers = musketeers.filter(musk => musk !== 'Aramis')
 for (const musketeer of musketeers) {
     console.log(musketeer)
 }
+
+// Sum of values
+
+const values = [3, 11, 7, 2, 9, 10];
+
+let sum = values.reduce((prev, current) => prev + current)
+
+console.log(sum)
+
+// Array Maximum
+
+const values = [3, 11, 7, 2, 9, 10];
+
+console.log(Math.max(...values))
