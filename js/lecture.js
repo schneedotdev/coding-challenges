48a87aa99d158678762b5825dcf34f603fba5b5f
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-22 19:36:03 -0400

    coding problems

---
+++ js/lecture.js
@@ -0,0 +1,10 @@
+// *Variables*
+// Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
+let holiday = 'Halloween';
+holiday = holiday.toUpperCase();
+console.log(holiday);
+
+//Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+let str = 'this is my string';
+let len = str.length;
+alert(str.substring(len - 4, len));
