704c8efe62c4fb108e537ca65088a7ba933f1854
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-24 15:33:11 -0400

    Alternate Capitalization

--- js/codewars.js
+++ js/codewars.js
@@ -210,3 +210,25 @@ function mergeArrays(arr1, arr2) {
   let uniqueNums = [...new Set(mergedNums)];
   return uniqueNums.sort((a, b) => a - b);
 }
+
+/**
+ * Name: Alternate capitalization
+ * Description: Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even. For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+ */
+
+function capitalize(s) {
+  let arr1 = [];
+  let arr2 = [];
+
+  s.split('').forEach((char, i) => {
+    if (i % 2 === 0) {
+      arr1.push(char.toUpperCase());
+      arr2.push(char.toLowerCase());
+    } else {
+      arr2.push(char.toUpperCase());
+      arr1.push(char.toLowerCase());
+    }
+  });
+
+  return [arr1.join(''), arr2.join('')];
+};
