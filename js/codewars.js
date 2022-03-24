c58413b27ff6982ca4a7ef9c2c543286a6c209ec
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-24 14:17:24 -0400

    Summing a number's digits

--- js/codewars.js
+++ js/codewars.js
@@ -189,4 +189,18 @@ function removeSmallest(numbers) {
 
 const solution = nums => nums !== null ? nums.sort((a, b) => a - b) : [];
 
+/**
+ * Name: Summing a number's digits
+ * Description: Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+ */
 
+
+function sumDigits(number) {
+  let str = String(Math.abs(number))
+  return Number(str.split('').reduce((a, b) => Number(a) + Number(b)))
+}
+
+/**
+ * Name: 
+ * Description: 
+ */
