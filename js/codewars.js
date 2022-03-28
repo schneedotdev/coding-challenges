2c4321d25cca2205a3b5ccb4ec00cc223217cbe3
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-28 19:05:14 -0400

    Decibel Scale formula

--- js/codewars.js
+++ js/codewars.js
@@ -264,6 +264,20 @@ var countSheep = function (num) {
 const stringToArray = string => string.split(' ')
 
 /**
+ * Name: Decibel Scale
+ * Description: Your task is to simply calculate the loudness of a sound wave, given its intensity as a parameter to the dBScale/db_scale function. Using the Decibel Scale Equation 
+ */
+
+const dBScale = intensity => 10 * Math.log10(intensity / 10 ** -12)
+
+
+
+
+
+
+
+
+/**
  * IN PROGRESS
  * Name: Dot Calculator
  * Description: 
