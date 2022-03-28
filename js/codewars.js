6ecb4792e3a037d50a897096306f65093780f5a7
Brian Schnee <brian.a.schnee@gmail.com>
2022-03-28 19:13:17 -0400

    find the first non-consecutive number

--- js/codewars.js
+++ js/codewars.js
@@ -270,6 +270,20 @@ const stringToArray = string => string.split(' ')
 
 const dBScale = intensity => 10 * Math.log10(intensity / 10 ** -12)
 
+/**
+ * Name: Find the first non-consecutive number
+ * Description: Your task is to find the first element of an array that is not consecutive. By not consecutive we mean not exactly 1 larger than the previous element of the array.
+ */
+
+function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + 1 !== arr[i + 1])
+      return arr[i + 1]
+  }
+
+  return null;
+}
+
 
 
 
