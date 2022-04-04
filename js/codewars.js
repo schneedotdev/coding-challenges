ea9aad1c8ee3c709d9b32103748b2f0fe7059826
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-04 02:37:43 -0400

    more codewars problems

--- js/codewars.js
+++ js/codewars.js
@@ -429,3 +429,43 @@ const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
  */
 
 const findShort = s => s.split(' ').reduce((a, b) => b.length < a.length ? b : a).length
+
+
+/**
+ * Name: Thinkful - String Drills: Repeater
+ * Description: Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+ */
+
+const repeater = (string, n) => string.repeat(n)
+
+/**
+ * Name: Alphabet war
+ * Description: Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+ */
+
+function alphabetWar(fight) {
+  const letters = {
+    's': function () { this.left++ },
+    'b': function () { this.left += 2 },
+    'p': function () { this.left += 3 },
+    'w': function () { this.left += 4 },
+    'z': function () { this.right++ },
+    'd': function () { this.right += 2 },
+    'q': function () { this.right += 3 },
+    'm': function () { this.right += 4 },
+    left: 0,
+    right: 0
+  }
+
+  fight.split('').forEach(letter => {
+    if (letter in letters)
+      letters[letter]()
+  })
+
+  if (letters.left > letters.right)
+    return "Left side wins!"
+  else if (letters.left < letters.right)
+    return "Right side wins!"
+  else
+    return "Let's fight again!"
+}
