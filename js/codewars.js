5e654cf8b509860bc00616f8f1dd096f102592a0
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-04 01:03:57 -0400

    convert a binary value to a number

--- js/codewars.js
+++ js/codewars.js
@@ -414,3 +414,11 @@ const closestMultiple10 = num => {
 
   return integer;
 };
+
+
+/**
+ * Name: Ones and Zeros
+ * Description: Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+ */
+
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
