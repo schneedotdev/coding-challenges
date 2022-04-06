e39a0b99645d3604eeae5f5f8b4746984d68d7c6
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-05 23:22:53 -0400

    Jaden Casing Strings problem

--- js/codewars.js
+++ js/codewars.js
@@ -488,6 +488,26 @@ function amountOfPages(summary) {
 }
 
 /**
+ * Name: Jaden Casing Strings
+ * Description: Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+ */
+
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map(word => {
+    const firstLetter = word[0]
+    return word.replace(firstLetter, firstLetter.toUpperCase())
+  }).join(' ')
+};
+
+
+
+
+
+
+
+
+
+/** IN PROGRESS
  * Name: Title Case
  * Description: Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
  */
