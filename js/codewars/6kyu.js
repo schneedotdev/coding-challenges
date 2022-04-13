855f57e57e204540605f49cfda8c4d766e15851c
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-13 02:54:08 -0400

    added a 6kyu and 7kyu problem

--- js/codewars/6kyu.js
+++ js/codewars/6kyu.js
@@ -30,3 +30,60 @@ Array.prototype.sum = _ => this.reduce((acc, cur) => acc + cur, 0);
 Array.prototype.even = _ => this.filter(num => num % 2 === 0);
 
 Array.prototype.odd = _ => this.filter(num => num % 2 !== 0);
+
+
+
+/**
+ * Name: Array.diff
+ * Description: It should remove all values from list a, which are present in list b keeping their order. If a value is present in b, all of its occurrences must be removed from the other
+ */
+
+const arrayDiff = (a, b) => b ? a.filter(num => !b.includes(num)) : a
+
+
+/**
+ * Name: Count characters in your string
+ * Description: The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}. What if the string is empty? Then the result should be empty object literal, {}.
+
+ */
+
+function count(string) {
+  let arr = []
+  let counter = {}
+
+  string.split('').forEach(char => {
+    if (!arr.includes(char)) {
+      arr.push(char)
+      counter[char] = 1
+    } else {
+      counter[char]++
+    }
+  });
+
+  return counter;
+}
+
+/**
+ * Name: Data Reverse
+ * Description: 
+ * 11111111  00000000  00001111  10101010
+ * (byte1)   (byte2)   (byte3)   (byte4)
+ *
+ * should become
+ *
+ * 10101010  00001111  00000000  11111111
+ * (byte4)   (byte3)   (byte2)   (byte1)
+ *
+ * Data is given as: [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+ */
+
+function dataReverse(data) {
+  let data_copy = []
+
+  data.reverse()
+
+  while (data.length)
+    data_copy.push(data.splice(0, 8).reverse());
+
+  return [].concat.apply([], data_copy);
+}
--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -146,3 +146,10 @@ function titleCase(title) {
 
     return arr.join(' ');
 }
+
+/**
+ * Name: Is this a triangle?
+ * Description: Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+ */
+
+const isTriangle = (a, b, c) => a + b > c && a + c > b && b + c > a;
