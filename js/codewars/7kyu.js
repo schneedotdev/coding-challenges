0323fa685c85c0ab47249fc1c2870b4d6d0eeb12
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-14 23:00:29 -0400

    7kyu

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -153,3 +153,19 @@ function titleCase(title) {
  */
 
 const isTriangle = (a, b, c) => a + b > c && a + c > b && b + c > a;
+
+/**
+ * Name: Filter unused digits
+ * Description: Given a varying number of integer arguments, return the digits that are not present in any of them. 
+ * [12, 34, 56, 78]  =>  "09"
+ * [2015, 8, 26]     =>  "3479"
+ */
+
+function unusedDigits(...nums) {
+    let check = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let missing = nums
+        .map(num => (num + '').split('').map(num => ~~num))
+        .flat(1)
+
+    return check.filter(num => !(missing.includes(num))).join('')
+}
