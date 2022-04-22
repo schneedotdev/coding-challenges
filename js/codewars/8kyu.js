de35c837edf83477483e61f73541697905874e2f
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-22 01:41:36 -0400

    convert a binary string to decimal

--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -408,3 +408,12 @@ var stringToNumber = str => ~~str
  */
 
 const countSheeps = arrayOfSheep => arrayOfSheep.filter(sheep => sheep).length;
+
+/**
+ * Name: Binary to Decimal
+ * Description: Convert a binary string to decimal
+ */
+
+function binToDec(bin) {
+    return bin.split('').reverse().map((num, i) => 2 ** i * num).reduce((a, b) => a + b, 0)
+}
