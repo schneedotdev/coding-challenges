9abe6b8af1e43326de6f7c0da13bc4c178d7b2cc
brianschnee <brian.a.schnee@gmail.com>
2022-04-21 23:17:52 -0400

    8kyu - I love you, a little, a lot problem

---
+++ 8kyu/8-i_love_you_alot.js
@@ -0,0 +1,13 @@
+/**
+ * Name: I love you, a little , a lot, passionately ... not at all
+ * Description:
+ *
+ * I love you
+ * a little
+ * a lot
+ * passionately
+ * madly
+ * not at all
+ *
+ * Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+ */
