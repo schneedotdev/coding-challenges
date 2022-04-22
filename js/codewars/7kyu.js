02f7bfb2ebf6ae28af5a5fe0851bc381e3a9510d
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-22 01:47:08 -0400

    reverse words

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -202,3 +202,12 @@ function findNextSquare(sq) {
 
     return sq;
 }
+
+/**
+ * Name: Reverse words
+ * Description: Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained. 
+ * "This is an example!" ==> "sihT si na !elpmaxe"
+ * "double  spaces"      ==> "elbuod  secaps"
+ */
+
+const reverseWords = str => str.split(' ').map(word => [...word].reverse().join('')).join(' ');
