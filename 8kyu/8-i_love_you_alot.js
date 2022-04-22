048fce6209bc51ef04be2bee5f517b86d2f5ee76
brianschnee <brian.a.schnee@gmail.com>
2022-04-21 23:19:05 -0400

    8kyu - I love you, a little, a lot solution

--- 8kyu/8-i_love_you_alot.js
+++ 8kyu/8-i_love_you_alot.js
@@ -11,3 +11,16 @@
  *
  * Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
  */
+
+const howMuchILoveYou = (i) => {
+	const circularArray = [
+		'I love you',
+		'a little',
+		'a lot',
+		'passionately',
+		'madly',
+		'not at all',
+	];
+	const n = circularArray.length;
+	return circularArray[(((i - 1) % n) + n) % n];
+};
