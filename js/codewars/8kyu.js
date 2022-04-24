c44306541030c847a0daaf5f6321f8a44dc82282
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-23 23:28:53 -0400

    8kyu year conversion problem

--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -427,3 +427,24 @@ const abbrevName = name => {
     const arr = name.toUpperCase().split(' ');
     return `${arr[0][0]}.${arr[1][0]}`
 }
+
+/**
+ * Name: Cat years, Dog years
+ * Description: convert human years into cat and dog years
+ */
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+    let catYears;
+    let dogYears;
+
+    if (humanYears === 1) {
+        catYears = 15;
+        dogYears = 15;
+    } else {
+        let mult = humanYears - 2;
+        dogYears = 24 + mult * 5;
+        catYears = 24 + mult * 4;
+    }
+
+    return [humanYears, catYears, dogYears];
+}
