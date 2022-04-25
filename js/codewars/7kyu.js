4706c34b5296be4bd1da81216edb0d85a57c3e13
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-25 02:10:39 -0400

    check to see if a word has multiple instances of the same letter

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -235,3 +235,15 @@ function printerError(s) {
     return `${s.split('')
                .reduce((a, b) => !'abcdefghijklm'.includes(b) ? a += 1 : a += 0, 0)}/${s.length}`
 }
+
+/**
+ * Name: Isograms
+ * Description: An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+ */
+
+const isIsogram = s => {
+    return Object.keys(s.split('').reduce((a, b) => {
+        a.found.includes(b.toLowerCase()) ? a[b] = b : a.found.push(b.toLowerCase());
+        return a;
+    }, { found: [] })).length === 1;
+};
