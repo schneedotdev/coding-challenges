94009e87b9cca4673f90605547d3cbcbb0c3992f
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-24 22:06:22 -0400

    Count positives, sum negatives

--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -448,3 +448,33 @@ var humanYearsCatYearsDogYears = function (humanYears) {
 
     return [humanYears, catYears, dogYears];
 }
+
+/**
+ * Name: Opposite Number
+ * Description: negate the sign
+ */
+
+function opposite(number) {
+    return number * -1
+}
+
+
+/**
+ * Name: Count of positives / sum of negatives
+ * Description: Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+ */
+
+function countPositivesSumNegatives(input) {
+    return !input || input.length === 0 ? [] : input.reduce((a, b) => {
+        if (b > 0)
+            a[0] += 1
+        else if (b < 0)
+            a[1] += b
+
+        return a
+    }, [0, 0]);
+}
