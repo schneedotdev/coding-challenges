35b7df73e1ef8c5f3e0004ea1debc48ecd46479c
Brian Schnee <brian.a.schnee@gmail.com>
2022-04-27 21:56:30 -0400

    invert sign

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -247,3 +247,12 @@ const isIsogram = s => {
         return a;
     }, { found: [] })).length === 1;
 };
+
+/**
+ * Name: Binary Addition
+ * Description: Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+ */
+
+function addBinary(a, b) {
+    return (a + b).toString(2)
+}
--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -553,3 +553,12 @@ function stringClean(s) {
  */
 
 let isSquare = n => Math.sqrt(n) % 1 === 0
+
+/**
+ * Name: Invert values
+ * Description: Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+ */
+
+function invert(array) {
+    return array.map(num => num * -1);
+}
