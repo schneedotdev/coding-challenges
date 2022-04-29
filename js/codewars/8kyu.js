31b71b58e5cff899321c7210989906d2537fb027
brianschnee <brian.a.schnee@gmail.com>
2022-04-28 23:54:54 -0400

    pluralized word

--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -562,3 +562,10 @@ let isSquare = n => Math.sqrt(n) % 1 === 0
 function invert(array) {
     return array.map(num => num * -1);
 }
+
+/**
+ * Name: Plural
+ * Description: We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+ */
+
+const plural = n => n !== 1;
