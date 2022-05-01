171580a3422c8951214b98233ad928b81004eda0
brianschnee <brian.a.schnee@gmail.com>
2022-05-01 01:11:33 -0400

    count the amount of times a character appears in a string that has a duplicate

--- js/codewars/6kyu.js
+++ js/codewars/6kyu.js
@@ -167,3 +167,19 @@ function longestConsec(strarr, k) {
 
   return longest;
 }
+
+/**
+ * Name: Counting Duplicates
+ * Description: Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+ */
+
+function duplicateCount(text) {
+  return Object.values(text.toLowerCase().split('').reduce((encountered, char) => {
+    if (char in encountered)
+      encountered[char]++;
+    else
+      encountered[char] = 0
+
+    return encountered;
+  }, {})).filter(key => key > 0).length;
+}
