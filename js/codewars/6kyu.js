f7137f98af6467c6582e05588cda2589a1f5adec
brianschnee <brian.a.schnee@gmail.com>
2022-05-05 14:43:52 -0400

    pyramid arrays

--- js/codewars/6kyu.js
+++ js/codewars/6kyu.js
@@ -198,3 +198,17 @@ var uniqueInOrder = function (iterable) {
   }
   return chars;
 }
+
+/**
+ * Name: Pyramid Array
+ * Description: Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+ */
+
+function pyramid(n) {
+  let arr = [];
+  
+  for(let i = 1; i < n + 1; i++)
+    arr.push(new Array(i).fill(1));
+  
+  return arr;
+}
