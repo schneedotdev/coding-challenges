dbadee66c9928d22044e1869841f132e3ff20042
brianschnee <brian.a.schnee@gmail.com>
2022-05-05 22:42:12 -0400

    description

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -268,7 +268,8 @@ function getEvenNumbers(numbersArray){
 
 /**
  * Name: Vowel Count
- * Description: 
+ * Description: Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or spaces.
+
  */
 
 function getCount(str) {
