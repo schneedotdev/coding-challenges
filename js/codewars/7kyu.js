3b41ba200cf76194e48c5dc2bf94b41e749c9632
brianschnee <brian.a.schnee@gmail.com>
2022-05-06 23:31:44 -0400

    Sum of numbers within range

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -292,3 +292,21 @@ Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, e
 function bingo(a) {
   return [2, 9, 14, 7, 15].filter(e => a.includes(e)).length === 5 ? 'WIN' : 'LOSE';
 }
+
+/**
+ * Name: Beginner Series #3 Sum of Numbers
+ * Description: Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+ * 
+ * Note: a and b are not ordered!
+ */
+
+function getSum(a,b) {
+  let range = [a, b].sort((a, b) => a - b);
+  let sum = 0;
+  
+  for(let i = range[0]; i <= range[1]; i++) {
+    sum += i;
+  }
+  
+  return sum;
+}
