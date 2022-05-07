d51e64a259f5f592b2ceba26acc6cc295aa19b28
brianschnee <brian.a.schnee@gmail.com>
2022-05-06 20:21:40 -0400

    bingo

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -288,3 +288,7 @@ function getCount(str) {
 Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array). Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
 
  */
+
+function bingo(a) {
+  return [2, 9, 14, 7, 15].filter(e => a.includes(e)).length === 5 ? 'WIN' : 'LOSE';
+}
