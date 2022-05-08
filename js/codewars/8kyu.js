9200019e1ce4241e945aaa932ae970b231d62f32
brianschnee <brian.a.schnee@gmail.com>
2022-05-08 13:55:46 -0400

    multiply based on odd or even

--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -639,3 +639,14 @@ function paperwork(n, m) {
     return 0;
   return n * m
 }
+
+/**
+ * Name: Simple Multiplication
+ * Description: This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+ */
+
+function simpleMultiplication(number) {
+  if(number % 2 === 0)
+    return number * 8;
+  return number * 9;
+}
