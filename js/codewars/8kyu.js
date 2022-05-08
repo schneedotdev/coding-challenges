b75b46c097d924ad0d076ba296314cd2df072c9b
brianschnee <brian.a.schnee@gmail.com>
2022-05-08 13:29:06 -0400

    school paperwork problem

--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -625,3 +625,17 @@ class SmallestIntegerFinder {
 function removeEveryOther(arr){
   return arr.filter((e, i) => i % 2 === 0)
 }
+
+/**
+ * Name: Series #1 School Paperwork
+ * Description: Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+ * Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+ * n= 5, m=5: 25
+ * n=-5, m=5:  0
+ */
+
+function paperwork(n, m) {
+  if(n < 0 || m < 0)
+    return 0;
+  return n * m
+}
