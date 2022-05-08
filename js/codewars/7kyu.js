4448e5f736bee25778bcb0f37174fbb76f61fcbf
brianschnee <brian.a.schnee@gmail.com>
2022-05-08 13:52:00 -0400

    accum problem

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -310,3 +310,25 @@ function getSum(a,b) {
   
   return sum;
 }
+
+/**
+ * Name: Mumbling
+ * Description: 
+ * accum("abcd") -> "A-Bb-Ccc-Dddd"
+ * accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+ * accum("cwAt") -> "C-Ww-Aaa-Tttt"    
+ */
+
+function accum(s) {
+    let arr = [];
+  
+    s.split('').forEach((c, i) => {
+        arr.push(c.repeat(i + 1));
+    });
+  
+    return arr.map(str => {
+        return str.split('')
+                .map((c, i) => (i === 0) ? c.toUpperCase() : c.toLowerCase())
+                .join('');
+    }).join('-')
+}
