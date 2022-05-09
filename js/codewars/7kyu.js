c1ff482099fcd8bfd8cf52701b78857bb5ea960b
brianschnee <brian.a.schnee@gmail.com>
2022-05-09 01:40:48 -0400

    updated implementation

--- js/codewars/7kyu.js
+++ js/codewars/7kyu.js
@@ -340,10 +340,7 @@ function accum(s) {
 
 function matrix(array) {
   for(let i = 0; i < array.length; i++) {
-    if(array[i][i] < 0)
-      array[i][i] = 0;
-    else
-      array[i][i] = 1;
+    array[i][i] = array[i][i] < 0 ? 0 : 1;
   }
   
   return array;
