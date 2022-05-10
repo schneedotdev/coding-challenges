bee812cfaa8f57e729e05217487e5940c2b5b247
brianschnee <brian.a.schnee@gmail.com>
2022-05-10 17:59:04 -0400

    capitlization method

--- js/codewars/8kyu.js
+++ js/codewars/8kyu.js
@@ -659,3 +659,12 @@ function simpleMultiplication(number) {
 function arrayPlusArray(arr1, arr2) {
   return [...arr1, ...arr2].reduce((a, b) => a + b, 0)
 }
+
+/**
+ * Name: Capitalization and Mutability
+ * Description: write a simple helper function to capitalize a string (that contains a single word)
+ */
+
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
