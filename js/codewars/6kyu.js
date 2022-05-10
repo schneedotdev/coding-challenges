54155684e0c636db64398132d546ebfa37656f1d
brianschnee <brian.a.schnee@gmail.com>
2022-05-10 18:18:39 -0400

    alphabet position

--- js/codewars/6kyu.js
+++ js/codewars/6kyu.js
@@ -212,3 +212,19 @@ function pyramid(n) {
   
   return arr;
 }
+
+/**
+ * Name: Replace With Alphabet Position
+ * Description: In this kata you are required to, given a string, replace every letter with its position in the alphabet. If anything in the text isn't a letter, ignore it and don't return it.
+ */
+
+function alphabetPosition(text) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return text.split('')
+              .map(char => {
+                if(alphabet.includes(char.toLowerCase()))
+                  return alphabet.indexOf(char.toLowerCase()) + 1;
+              })
+              .filter(e => e !== undefined)
+              .join(' ');
+}
