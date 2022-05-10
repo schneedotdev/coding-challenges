75f7921bc3a9c0c3c929d0bc5931cbc40e4e26ba
brianschnee <brian.a.schnee@gmail.com>
2022-05-10 18:20:08 -0400

    stylization change

--- js/codewars/6kyu.js
+++ js/codewars/6kyu.js
@@ -220,11 +220,12 @@ function pyramid(n) {
 
 function alphabetPosition(text) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  return text.split('')
-              .map(char => {
-                if(alphabet.includes(char.toLowerCase()))
-                  return alphabet.indexOf(char.toLowerCase()) + 1;
-              })
-              .filter(e => e !== undefined)
-              .join(' ');
+  return text
+    .split('')
+    .map(char => {
+      if(alphabet.includes(char.toLowerCase()))
+        return alphabet.indexOf(char.toLowerCase()) + 1;
+    })
+    .filter(e => e !== undefined)
+    .join(' ');
 }
