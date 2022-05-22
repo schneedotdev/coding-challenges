18348e1a5cb1378f3283fb7f20a7e4b71982ee9c
brianschnee <brian.a.schnee@gmail.com>
2022-05-22 00:36:02 -0400

    Sum of differences description

--- 8kyu/8-sum_of_differences.js
+++ 8kyu/8-sum_of_differences.js
@@ -1,3 +1,9 @@
+/**
+ * Name: Sum of differences in array
+ * Description: Your task is to sum the differences between consecutive pairs in the array in descending order.
+ * Link: https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/train/javascript
+ */
+
 function sumOfDifferences(arr) {
 	if (!arr.length) return 0;
 
