feea1790105200bfcaf9e71fc4a2215517481a40
brianschnee <brian.a.schnee@gmail.com>
2022-05-22 00:48:24 -0400

    what is between description

--- 8kyu/8-what_is_between.js
+++ 8kyu/8-what_is_between.js
@@ -1,3 +1,9 @@
+/**
+ * Name: What is between?
+ * Description: Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+ * Link: https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/javascript
+ */
+
 function between(a, b) {
 	let arr = [a];
 
