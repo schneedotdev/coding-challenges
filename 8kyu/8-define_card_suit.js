7a6b26f62c674c80b9c7f7ff794f190a48c4929b
brianschnee <brian.a.schnee@gmail.com>
2022-05-25 18:45:55 -0400

    Define a Card Suit

---
+++ 8kyu/8-define_card_suit.js
@@ -0,0 +1,11 @@
+/**
+ * Name: Define a Card Suit
+ * Description: You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+ *
+ * ('3♣') -> return 'clubs'
+ * ('3♦') -> return 'diamonds'
+ * ('3♥') -> return 'hearts'
+ * ('3♠') -> return 'spades'
+ *
+ * Link: https://www.codewars.com/kata/5a360620f28b82a711000047/train/javascript
+ */
