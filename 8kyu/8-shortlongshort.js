7ff22e8bfce7494d3e13e6bb98905cbd4ffa7b7d
brianschnee <brian.a.schnee@gmail.com>
2022-05-26 18:40:37 -0400

    codewars

--- 8kyu/8-shortlongshort.js
+++ 8kyu/8-shortlongshort.js
@@ -1,3 +1,10 @@
+/**
+ * Name: Short Long Short
+ * Description: Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+ *
+ * Link: https://www.codewars.com/kata/50654ddff44f800200000007/train/javascript
+ */
+
 function solution(a, b) {
 	const arr = [a, b].sort((a, b) => a.length - b.length, 0);
 	return arr[0] + arr[1] + arr[0];
