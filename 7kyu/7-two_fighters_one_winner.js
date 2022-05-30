596146d6e5e61eed3fe10d7c907f4d1afe960af3
brianschnee <brian.a.schnee@gmail.com>
2022-05-29 23:02:29 -0400

    description 7kyu

--- 7kyu/7-two_fighters_one_winner.js
+++ 7kyu/7-two_fighters_one_winner.js
@@ -1,3 +1,12 @@
+/**
+ * Name: Two fighters, one winner.
+ * Description: Create a function that returns the name of the winner in a fight between two fighters.
+ * Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+ *
+ * Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+ *
+ * Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+ */
 function declareWinner(fighter1, fighter2, firstAttacker) {
 	let first = {};
 	let second = {};
