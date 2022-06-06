5ce0c10d4e78cb740935c4b47b6b283a827c0516
brianschnee <brian.a.schnee@gmail.com>
2022-06-06 15:36:01 -0400

    messi goals

---
+++ 8kyu/8-messi_goals.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Grasshopper - Messi goals function
+ * Description: Complete the function to return his total number of goals in all three leagues.
+ * Link: https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+ */
+
+const goals = (a, b, c) => a + b + c;
