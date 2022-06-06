cf2d3a4fe9b4f735f16d7ec46c85269af072ea12
brianschnee <brian.a.schnee@gmail.com>
2022-06-06 15:47:18 -0400

    quarter of the year refactor

--- 8kyu/8-quarter_of_year.js
+++ 8kyu/8-quarter_of_year.js
@@ -4,5 +4,4 @@
  * Link: https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/javascript
  */
 
-const quarterOf = (month) =>
-	month <= 3 ? 1 : month <= 6 ? 2 : month <= 9 ? 3 : 4;
+const quarterOf = (month) => Math.ceil(month / 3);
