4b27624e0ba8e33bdbd717f4a4c083918553b3f2
brianschnee <brian.a.schnee@gmail.com>
2022-06-08 22:11:56 -0400

    6kyu description and link

--- 6kyu/6-pair_of_gloves.js
+++ 6kyu/6-pair_of_gloves.js
@@ -1,3 +1,10 @@
+/**
+ * Name: Pair of gloves
+ * Description: The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+ */
+
 function numberOfPairs(gloves) {
 	const count = {};
 
