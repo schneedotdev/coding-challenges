d31daa4fa654b638a62315b0ea093a75607a7d4e
brianschnee <brian.a.schnee@gmail.com>
2022-06-24 23:39:15 -0400

    string template bug fixes

---
+++ 8kyu/8-template_bug_fixes.js
@@ -0,0 +1,9 @@
+/**
+ * Name: String Template Bug Fixes
+ * Description: Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+ * Link: https://www.codewars.com/kata/55c90cad4b0fe31a7200001f/train/javascript
+ */
+
+function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
