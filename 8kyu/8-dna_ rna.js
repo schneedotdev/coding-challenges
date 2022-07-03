10ccc6ddbb57c9596d80cafd7ddbe8955f9a0612
brianschnee <brian.a.schnee@gmail.com>
2022-07-02 23:37:49 -0400

    8kyu - dna to rna

---
+++ 8kyu/8-dna_ rna.js
@@ -0,0 +1,12 @@
+/**
+ * Name: DNA to RNA conversion
+ * Description: For every instance of nucleaic acid base Thymine, replace it with Uracil
+ * Link: https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
+ */
+
+const DNAtoRNA = (dna) => {
+	return dna
+		.split('')
+		.map((base) => (base === 'T' ? 'U' : base))
+		.join('');
+};
