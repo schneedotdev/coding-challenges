844820ef50ad0021f4b4226ebd19492b95a6b449
brianschnee <brian.a.schnee@gmail.com>
2022-07-02 23:44:10 -0400

    8kyu - check for factor

---
+++ 8kyu/8-check_for_factor.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Check for factor
+ * Description: This function should test if the factor is a factor of base.
+ * Link: https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+ */
+
+const checkForFactor = (base, factor) => base % factor < 1;
