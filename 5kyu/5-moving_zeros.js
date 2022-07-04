0dd2960fcf90f54188aa38d93b2654d5ddd25988
brianschnee <brian.a.schnee@gmail.com>
2022-07-04 01:49:09 -0400

    5kyu - moving zeros to the end

---
+++ 5kyu/5-moving_zeros.js
@@ -0,0 +1,18 @@
+/**
+ * Name: Moving zeros to the end
+ * Description: Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+ * Link: https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+ */
+
+function moveZeros(arr) {
+	const zeros = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === 0) {
+			zeros.push(arr.splice(i, 1)[0]);
+			i--;
+		}
+	}
+
+	return arr.concat(zeros);
+}
