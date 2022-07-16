152d73ff046842302b454ae67a7b0d8d356420de
brianschnee <brian.a.schnee@gmail.com>
2022-07-16 14:32:17 -0400

    boolean to string

---
+++ 8kyu/8-boolean_to_string.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Convert a Boolean to a String
+ * Description: Implement a function which convert the given boolean value into its string representation.
+ * Link: https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+ */
+
+const booleanToString = b => b + ''
