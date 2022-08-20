cc43533c808a8547b5c23b2a27bf423cdd47dd60
brianschnee <brian.a.schnee@gmail.com>
2022-08-20 11:00:01 -0400

    8kyu - Welcome

---
+++ 8kyu/8-welcome.js
@@ -0,0 +1,37 @@
+/**
+ * Name: Welcome!
+ * Description: 
+ * - Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+ * - Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+ * Link: https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/javascript
+ */
+
+const map = new Map();
+
+function initMap(arr) {
+    arr.forEach(e => map.set(e[0], e[1]))
+}
+
+function greet(language) {
+    return map.get(language) || map.get('english')
+}
+
+initMap([
+    ['english', 'Welcome'],
+    ['czech', 'Vitejte'],
+    ['danish', 'Velkomst'],
+    ['dutch', 'Welkom'],
+    ['estonian', 'Tere tulemast'],
+    ['finnish', 'Tervetuloa'],
+    ['flemish', 'Welgekomen'],
+    ['french', 'Bienvenue'],
+    ['german', 'Willkommen'],
+    ['irish', 'Failte'],
+    ['italian', 'Benvenuto'],
+    ['latvian', 'Gaidits'],
+    ['lithuanian', 'Laukiamas'],
+    ['polish', 'Witamy'],
+    ['spanish', 'Bienvenido'],
+    ['swedish', 'Valkommen'],
+    ['welsh', 'Croeso']
+])
