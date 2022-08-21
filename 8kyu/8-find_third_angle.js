1825d32c4e5a8b0bb649beb7e98bf759dc46e905
brianschnee <brian.a.schnee@gmail.com>
2022-08-21 14:18:28 -0400

    8kyu - third angle of triangle

---
+++ 8kyu/8-find_third_angle.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Third Angle of a Triangle
+ * Description: You are given two interior angles (in degrees) of a triangle. Write a function to return the 3rd.
+ * Link: https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+ */
+
+const otherAngle = (a, b) => 180 - a - b;
