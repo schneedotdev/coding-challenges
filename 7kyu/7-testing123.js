890521945f56eb4821b88840ab4d564c7575252b
brianschnee <brian.a.schnee@gmail.com>
2022-09-08 22:24:55 -0400

    7kyu - Testing 1-2-3

---
+++ 7kyu/7-testing123.js
@@ -0,0 +1,7 @@
+/**
+ * Name: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+ * Description: Write a function which takes a list of strings and returns each line prepended by the correct number. The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+ * Link: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+ */
+
+var number = arr => arr.map((e, i) => `${i + 1}: ${e}`)
