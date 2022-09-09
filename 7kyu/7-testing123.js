9497ca7468562582d8756bf5fdc8abc99f869cb3
brianschnee <brian.a.schnee@gmail.com>
2022-09-08 22:24:59 -0400

    7kyu - Testing 1-2-3

--- 7kyu/7-testing123.js
+++ 7kyu/7-testing123.js
@@ -1,5 +1,5 @@
 /**
- * Name: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+ * Name: Testing 123
  * Description: Write a function which takes a list of strings and returns each line prepended by the correct number. The numbering starts at 1. The format is n: string. Notice the colon and space in between.
  * Link: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
  */
