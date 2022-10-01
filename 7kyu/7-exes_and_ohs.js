9b218f932699056ee3a9b5d4f0c5bf0950055dea
brianschnee <brian.a.schnee@gmail.com>
2022-10-01 12:43:52 -0400

    test

--- 7kyu/7-exes_and_ohs.js
+++ 7kyu/7-exes_and_ohs.js
@@ -3,7 +3,6 @@
  * Description: Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
  * Link: https://www.codewars.com/kata/55908aad6620c066bc00002a/train/javascript
  */
-
 function XO(str) {
     if (str === '') return true
 
