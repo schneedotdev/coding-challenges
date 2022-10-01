7f26bd08e66f27913179148f3ef3f326c45d5273
brianschnee <brian.a.schnee@gmail.com>
2022-10-01 12:39:10 -0400

    7kyu - exes and ohs

---
+++ 7kyu/7-exes_and_ohs.js
@@ -0,0 +1,12 @@
+/**
+ * Name: Exes and Ohs
+ * Description: Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+ * Link: https://www.codewars.com/kata/55908aad6620c066bc00002a/train/javascript
+ */
+
+function XO(str) {
+    let a = str.toLowerCase().match(/x/g)
+    let b = str.toLowerCase().match(/o/g)
+
+    return a && b ? a.length === b.length : false
+}
