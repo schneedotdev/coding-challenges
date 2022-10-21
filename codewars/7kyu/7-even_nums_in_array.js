43dcffc0e5091f973217d54a5c4e7d71a54b7169
brianschnee <brian.a.schnee@gmail.com>
2022-10-21 01:45:29 -0400

    7kyu - Even Numbers in Array refactor

--- codewars/7kyu/7-even_nums_in_array.js
+++ codewars/7kyu/7-even_nums_in_array.js
@@ -4,7 +4,20 @@
  * Link: https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c/train/javascript
  */
 
+// function evenNumbers(array, number) {
+//     let even = array.filter(num => num % 2 === 0)
+//     return even.slice(even.length - number)
+// }
+
 function evenNumbers(array, number) {
-    let even = array.filter(num => num % 2 === 0)
-    return even.slice(even.length - number)
+    let result = []
+
+    for (let i = array.length - 1; number > 0 && i >= 0; i--) {
+        if (array[i] % 2 === 0) {
+            result.push(array[i])
+            number--
+        }
+    }
+
+    return result.reverse()
 }
