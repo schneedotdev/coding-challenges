b4e591078962baa0b2cde31665d3e6a0f261ef7f
brianschnee <brian.a.schnee@gmail.com>
2022-10-26 18:46:30 -0400

    Reverse the casing of characters in a string and the order of words

---
+++ misc/string_transformer.js
@@ -0,0 +1,24 @@
+/**
+ * Name: String Transformer
+ * Description: Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.
+ */
+
+function stringTransformer(str) {
+    const reversed = str.split(' ').reverse().join(' ')
+    const switchedCase = [...reversed].map(char => {
+        if (char === char.toUpperCase())
+            return char.toLowerCase()
+        return char.toUpperCase()
+    }).join('')
+
+    return switchedCase
+}
