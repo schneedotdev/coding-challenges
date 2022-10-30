f771b4a531f060271230ce8c7b854662422d4f04
brianschnee <brian.a.schnee@gmail.com>
2022-10-30 02:53:29 -0400

    Sort by last char

---
+++ misc/sort_by_last_char.js
@@ -0,0 +1,11 @@
+/**
+ * Name: Sort by last char
+ * Description: Sort a string that contains words by the last letter of a word
+ */
+
+function sortByLastChar(x) {
+    return x.split(' ')
+        .sort((a, b) => {
+            return a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1)
+        })
+}
