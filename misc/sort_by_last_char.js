f431e8ec12ccd55e1f078b42ef676e2cf60628ee
brianschnee <brian.a.schnee@gmail.com>
2022-10-31 02:54:38 -0400

    Sort by last char alternate solution

--- misc/sort_by_last_char.js
+++ misc/sort_by_last_char.js
@@ -3,9 +3,16 @@
  * Description: Sort a string that contains words by the last letter of a word
  */
 
+// function sortByLastChar(x) {
+//     return x.split(' ')
+//         .sort((a, b) => {
+//             return a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1)
+//         })
+// }
+
 function sortByLastChar(x) {
     return x.split(' ')
         .sort((a, b) => {
-            return a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1)
+            return a.charAt(a.length - 1).localeCompare(b.charAt(b.length - 1))
         })
 }
