c8f749e74ad04b6e6404aebf041d32774003b5e3
brianschnee <brian.a.schnee@gmail.com>
2022-11-01 19:12:07 -0400

    8kyu - take the first N elements

---
+++ codewars/8kyu/8-first_n_numbers.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Enumerable Magic #25 - Take the First N Elements
+ * Description: Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+ * Link: https://www.codewars.com/kata/545afd0761aa4c3055001386/train/javascript
+ */
+
+const take = (arr, n) => arr.slice(0, n)
