efabda0974d9480426659fe60b288ab0e01ec014
brianschnee <brian.a.schnee@gmail.com>
2022-11-11 18:43:49 -0500

    7kyu - find the capitals. alternate solution

--- codewars/7kyu/7-find_caps.js
+++ codewars/7kyu/7-find_caps.js
@@ -4,11 +4,20 @@
  * Link: https://www.codewars.com/kata/539ee3b6757843632d00026b/train/javascript
  */
 
+// var capitals = function (word) {
+//     let arr = []
+//     for (let i = 0; i < word.length; i++) {
+//         if (word[i] === word[i].toUpperCase())
+//             arr.push(i)
+//     }
+//     return arr
+// };
+
 var capitals = function (word) {
-    let arr = []
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] === word[i].toUpperCase())
-            arr.push(i)
-    }
-    return arr
+    return [...word].reduce((indexes, letter, i) => {
+        if (letter === letter.toUpperCase()) {
+            indexes.push(i)
+        }
+        return indexes
+    }, [])
 };
