a9a6b8828ea8219d15a70bc61541126bc0bc1893
brianschnee <brian.a.schnee@gmail.com>
2022-11-13 18:50:59 -0500

    8kyu - String Repeat

---
+++ codewars/8kyu/8-string_repeat.js
@@ -0,0 +1,7 @@
+/**
+ * Name: String repeat
+ * Description: Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+ * Link: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+ */
+
+const repeatStr = (n, s) => s.repeat(n)
