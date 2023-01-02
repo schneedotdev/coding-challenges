9c40c17c589f71d209a604c054323233e777e1c3
brianschnee <brian.a.schnee@gmail.com>
2023-01-01 22:05:15 -0500

    8kyu - Is it even?

---
+++ codewars/8kyu/8-is_even.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Is it even?
+ * Description: In this Kata we are passing a number (n) into a function. Your code will determine if the number passed is even (or not). The function needs to return either a true or false. Numbers may be positive or negative, integers or floats. Floats with decimal part non equal to zero are considered UNeven for this kata.
+ * Link: https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript
+ */
+
+const testEven = n => n % 2 === 0
