df6f952ffa35bc7689dd12c4866e254e4ea07959
brianschnee <brian.a.schnee@gmail.com>
2023-01-08 22:16:41 -0500

    8kyu - Does my list include this?

---
+++ codewars/8kyu/8-list_includes.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Enumerable Magic - Does My List Include This?
+ * Description: Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+ * Link: https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
+ */
+
+const include = (arr, item) => arr.includes(item)
