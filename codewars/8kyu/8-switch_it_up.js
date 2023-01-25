20dd6ba0af7ad855f56e201f076fa3283b20bc38
brianschnee <brian.a.schnee@gmail.com>
2023-01-25 13:30:47 -0500

    8kyu - switch it up

---
+++ codewars/8kyu/8-switch_it_up.js
@@ -0,0 +1,8 @@
+/**
+ * Name: Switch it up
+ * Description: When provided with a number between 0-9, return it in words. Input :: 1, Output :: "One".
+ * Link: https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+ */
+
+const switchItUp = num => (['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+)[num]
