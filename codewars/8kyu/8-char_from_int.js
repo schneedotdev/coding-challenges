9a826c8b655cbcfd7bc0a7ec2aaaf635070449b5
brianschnee <brian.a.schnee@gmail.com>
2023-02-03 14:20:46 -0500

    8kyu - get char from ascii val

---
+++ codewars/8kyu/8-char_from_int.js
@@ -0,0 +1,7 @@
+/**
+ * Name: get character from ASCII Value
+ * Description: Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+ * Link: https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+ */
+
+const getChar = c => String.fromCharCode(c)
