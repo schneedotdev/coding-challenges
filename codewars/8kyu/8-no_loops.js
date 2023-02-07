a8c7b02f177d7238a628a4d3b9a7756de0f3f18f
brianschnee <brian.a.schnee@gmail.com>
2023-02-07 14:53:34 -0500

    8kyu - No loops, you only need one

---
+++ codewars/8kyu/8-no_loops.js
@@ -0,0 +1,7 @@
+/**
+ * Name: No loops - you only need one
+ * Description: You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+ * Link: https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce/train/javascript
+ */
+
+const check = (a,x) => a.includes(x);
