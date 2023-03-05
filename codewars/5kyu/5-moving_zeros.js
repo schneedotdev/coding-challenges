7527f531c05cf3942e3fbbf690833df3ef299902
brianschnee <brian.a.schnee@gmail.com>
2023-03-05 14:48:26 -0500

    5kyu - Moving zeros to the end

--- codewars/5kyu/5-moving_zeros.js
+++ codewars/5kyu/5-moving_zeros.js
@@ -22,3 +22,14 @@ function moveZeros2(nums) {
   const diff = nums.length - arr.length
   return arr.concat([...'0'.repeat(diff)].map(Number))
 }
+
+function moveZeros3(nums) {
+  const arrays = nums.reduce((arrays, num) => {
+		if(num) arrays.nonZeros.push(num)
+		else arrays.zeros.push(0)
+
+		return arrays
+	}, { nonZeros: [], zeros: [] })
+
+  return arrays.nonZeros.concat(arrays.zeros)
+}
