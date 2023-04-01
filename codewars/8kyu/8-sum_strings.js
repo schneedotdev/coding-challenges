9321d49fd30bb382045ddbd8a1aad3bf28b23256
brianschnee <brian.a.schnee@gmail.com>
2023-04-01 16:26:30 -0400

    8kyu - Sum the strings

---
+++ codewars/8kyu/8-sum_strings.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Sum The Strings
+ * Description: Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string)
+ * Link: https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+ */
+
+const sumStr = (a, b) => +a + +b + '';
