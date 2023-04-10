ba376861665d6174852bab60cf008ddcd7837bd7
brianschnee <brian.a.schnee@gmail.com>
2023-04-10 14:40:17 -0400

    8kyu - Remove Exclamation Marks

---
+++ codewars/8kyu/8-remove_exclamations.js
@@ -0,0 +1,9 @@
+/**
+ * Name: Remove exclamation marks
+ * Description: Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+ * Link: https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+ */
+
+function removeExclamationMarks(s) {
+	return s.split('!').join('');
+}
