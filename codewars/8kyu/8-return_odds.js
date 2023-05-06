bf0ea41336a7e3460f871f45b1900efa8d7f5fad
brianschnee <brian.a.schnee@gmail.com>
2023-05-06 14:51:49 -0400

    8kyu - Return Odds

---
+++ codewars/8kyu/8-return_odds.js
@@ -0,0 +1,9 @@
+/**
+ * Name: Are arrow functions odd?
+ * Description: Return the odds from a list
+ * Link: https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/train/javascript
+ */
+
+function odds(values) {
+	return values.filter((value) => value % 2);
+}
