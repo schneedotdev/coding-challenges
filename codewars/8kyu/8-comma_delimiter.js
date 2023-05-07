77d987a6730a65b9f76f02a3f53c4eb99f631af3
brianschnee <brian.a.schnee@gmail.com>
2023-05-07 14:54:06 -0400

    8kyu - Comma Delimiters

---
+++ codewars/8kyu/8-comma_delimiter.js
@@ -0,0 +1,7 @@
+/**
+ * Name: Printing Array elements with Comma delimiters
+ * Description: Convert an Array to a String consisting of each element joined by commas
+ * Link: https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+ */
+
+const printArray = (array) => array.join(',');
