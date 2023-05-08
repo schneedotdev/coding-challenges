ffe1df787b1d5140400aac7c05ad4257249bd1c5
brianschnee <brian.a.schnee@gmail.com>
2023-05-08 14:57:42 -0400

    7kyu - no oddities

---
+++ codewars/7kyu/7-no_oddities.js
@@ -0,0 +1,7 @@
+/**
+ * Name: No Oddities Here
+ * Description: return all even numbers from an array
+ * Link: https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+ */
+
+const noOdds = (values) => values.filter((value) => value % 2 === 0);
