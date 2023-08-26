611c421314d91aca34070ed708f72c9db6c70f42
brianschnee <brian.a.schnee@gmail.com>
2023-08-25 20:30:03 -0400

    11 - mostWater

--- leetcode/medium/11-mostWater.ts
+++ leetcode/medium/11-mostWater.ts
@@ -8,17 +8,40 @@ Return the maximum amount of water a container can store.
  * Link: https://leetcode.com/problems/container-with-most-water/description/
  */
 
+// function maxArea(height: number[]): number {
+//     const maxValues: number[] = [];
+
+//     for (let i = 0; i < height.length - 1; i++) {
+//         const values: number[] = [];
+//         for (let j = i + 1; j < height.length; j++) {
+//             const min = Math.min(height[i], height[j]);
+//             values.push(min * (j - i));
+//         }
+//         maxValues.push(Math.max(...values));
+//     }
+
+//     return Math.max(...maxValues);
+// };
+
 function maxArea(height: number[]): number {
-    const maxValues: number[] = [];
+	let maxValue = 0;
 
-    for (let i = 0; i < height.length - 1; i++) {
-        const values: number[] = [];
-        for (let j = i + 1; j < height.length; j++) {
-            const min = Math.min(height[i], height[j]);
-            values.push(min * (j - i));
-        }
-        maxValues.push(Math.max(...values));
-    }
+	for (let i = 0; i < height.length - 1; i++) {
+		let max = 0;
 
-    return Math.max(...maxValues);
-};
+		for (let j = i + 1; j < height.length; j++) {
+			const min = Math.min(height[i], height[j]);
+			const cur = min * (j - i);
+
+			if (cur > max) {
+				max = cur;
+			}
+		}
+
+		if (max > maxValue) {
+			maxValue = max;
+		}
+	}
+
+	return maxValue;
+}
