518885f54595c11b7ec09eec5293f5c305e32db8
brianschnee <brian.a.schnee@gmail.com>
2023-08-28 20:31:59 -0400

    11 - mostWater, c solution 2

--- leetcode/medium/11-mostWater/mostWater.c
+++ leetcode/medium/11-mostWater/mostWater.c
@@ -1,4 +1,23 @@
-#define min(x, y) (x < y ? x : y)
+// #define min(x, y) (x < y ? x : y)
+
+// int maxArea(int* height, int heightSize) {
+//     int result = 0;
+//     int left = 0;
+//     int right = heightSize - 1;
+
+//     while (left < right) {
+//         int area = (right - left) * min(height[left], height[right]);
+//         result = (area > result) ? area : result;
+
+//         if (height[left] < height[right]) {
+//             left++;
+//         } else {
+//             right--;
+//         }
+//     }
+
+//     return result;
+// }
 
 int maxArea(int* height, int heightSize) {
     int result = 0;
@@ -6,7 +25,15 @@ int maxArea(int* height, int heightSize) {
     int right = heightSize - 1;
 
     while (left < right) {
-        int area = (right - left) * min(height[left], height[right]);
+        int min = 0;
+
+        if (height[left] < height[right]) {
+            min = height[left];
+        } else {
+            min = height[right];
+        }
+
+        int area = (right - left) * min;
         result = (area > result) ? area : result;
 
         if (height[left] < height[right]) {
